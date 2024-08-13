9b968ecc5a883012d911de4672f211f3129c24fd
Sandeep <Sandyb2711@gmail.com>
2024-08-14 01:50:15 +0530

    added rest controller

--- src/main/java/com/devops/devops_automation/DevopsAutomationApplication.java
+++ src/main/java/com/devops/devops_automation/DevopsAutomationApplication.java
@@ -2,12 +2,26 @@ package com.devops.devops_automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DevopsAutomationApplication {
+@RestController
+@RequestMapping(value = "/hello")
+public class DevopsAutomationApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DevopsAutomationApplication.class, args);
+	}
+	
+	@GetMapping
+	public String helloWorld() 
+	{
+		return "APPLICATION UP AND RUNNING";
+		
 	}
 
 }
