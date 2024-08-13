d0dd0c3368af10dab521d8dc2f0a77aaf1987a97
Sandeep <Sandyb2711@gmail.com>
2024-08-14 01:30:35 +0530

    project setup on github for devops automation

---
+++ src/test/java/com/devops/devops_automation/DevopsAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devops_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
